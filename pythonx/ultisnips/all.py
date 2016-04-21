955b38c6f308511006394c6edb9fc202a293f19a
valeth <patrick.auernig@gmail.com>
2016-04-21 22:18:14 +0200

    remove useless arguments in getWPAPassphraseHash

--- pythonx/ultisnips/all.py
+++ pythonx/ultisnips/all.py
@@ -13,12 +13,14 @@ def getUsername(username, gecos = True, passwd = "/etc/passwd"):
 
 def getWPAPassphraseHash(ssid, passphrase):
     if len(passphrase) >= 8:
-        cmds = ["wpa_passphrase", ssid, passphrase, "|", "grep", "-E", "'^\s*psk.*'", "|", "cut", "-d=", "-f2"]
-        proc = subprocess.Popen(cmds, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(["wpa_passphrase", ssid, passphrase], stdout=subprocess.PIPE)
         proc.wait()
         for line in proc.stdout.readlines():
             match = re.match("^\s*psk.*$", line.decode("utf-8"))
             if match:
                 return match.string.split("=")[1].strip()
     return ""
+
+if __name__ == "__main__":
+    print(getWPAPassphraseHash("MySSID", "MySecurePassword"))
 
