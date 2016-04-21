d0b0ba634b83b0e9007ba7f871a5065ad64fdb82
valeth <patrick.auernig@gmail.com>
2016-04-21 22:11:26 +0200

    add snippet and script for wpa_supplicant.conf

--- pythonx/ultisnips/all.py
+++ pythonx/ultisnips/all.py
@@ -1,3 +1,6 @@
+import subprocess
+import re
+
 def getUsername(username, gecos = True, passwd = "/etc/passwd"):
     content = [x.split(":") for x in open(passwd, "r").readlines()]
 
@@ -7,4 +10,15 @@ def getUsername(username, gecos = True, passwd = "/etc/passwd"):
                 return entry[4]
             else:
                 return username
+
+def getWPAPassphraseHash(ssid, passphrase):
+    if len(passphrase) >= 8:
+        cmds = ["wpa_passphrase", ssid, passphrase, "|", "grep", "-E", "'^\s*psk.*'", "|", "cut", "-d=", "-f2"]
+        proc = subprocess.Popen(cmds, stdout=subprocess.PIPE)
+        proc.wait()
+        for line in proc.stdout.readlines():
+            match = re.match("^\s*psk.*$", line.decode("utf-8"))
+            if match:
+                return match.string.split("=")[1].strip()
+    return ""
 
