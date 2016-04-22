2e3ed1455fb4823a6b11ea14071b432b67d845c3
valeth <patrick.auernig@gmail.com>
2016-04-22 09:55:44 +0200

    add note type selector script for c

--- pythonx/ultisnips/c.py
+++ pythonx/ultisnips/c.py
@@ -14,3 +14,13 @@ def returnval(t):
     else:
         return "return NULL;"
 
+def getNoteCandidates(t):
+    options = ["TODO", "FIXME", "XXX"]
+    if t:
+        options = [ x[len(t):] for x in options if x.startswith(t) ]	
+
+    if len(options) == 1:
+        return options[0]
+    
+    return "[" + ",".join(options) + "]"
+
