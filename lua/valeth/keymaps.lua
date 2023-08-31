2043fd6caadd5616e5d14f97fa5eca01a0bc12b3
Patrick Auernig <patrick.auernig@mykolab.com>
2023-08-31 17:47:22 +0200

    Remap macro record and add more ergonomic insert mode exit

--- lua/valeth/keymaps.lua
+++ lua/valeth/keymaps.lua
@@ -1,21 +1,27 @@
-vim.keymap.set("n", "<Leader><CR>", "<cmd>noh<CR>")
-vim.keymap.set("n", "U", "<cmd>redo<CR>")
-vim.keymap.set("n", "Y", "y$")
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-vim.keymap.set({"n", "v"}, "H", "^")
-vim.keymap.set({"n", "v"}, "L", "$")
-vim.keymap.set({"n", "v", "i"}, "<C-s>", "<cmd>w<CR>")
+local map = vim.keymap.set
 
-vim.keymap.set({"n", "v"}, "<F1>", "<NOP>")
-vim.keymap.set("n", "s", "<NOP>")
+map("n", "<Leader><CR>", "<cmd>noh<CR>")
+map("n", "U", "<cmd>redo<CR>")
+map("n", "Y", "y$")
+map("n", "j", "gj")
+map("n", "k", "gk")
 
+map({"n", "v"}, "H", "^")
+map({"n", "v"}, "L", "$")
 
--- Some nice remaps (thanks Prime)
+map({"n", "v", "i"}, "<C-s>", "<cmd>w<CR>")
+map("n", "s", "<NOP>")
+
+-- Exit insert mode by quickly pressing jk
+map("i", "jk", "<ESC>")
+
+-- This avoids accidentially recording a macro
+map({"n", "v"}, "<F1>", "q")
+map({"n", "v"}, "q", "<NOP>")
 
 -- Move selected lines up or down
-vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv")
+map("v", "J", ":move '>+1<CR>gv=gv")
+map("v", "K", ":move '<-2<CR>gv=gv")
 
 -- Join lines without moving the cursor
-vim.keymap.set("n", "J", "mzJ`z")
+map("n", "J", "mzJ`z")
