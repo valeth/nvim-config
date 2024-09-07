a9cbdad4a6b96ebe0f54f3f309f417f3eb128916
Patrick Auernig <patrick.auernig@mykolab.com>
2024-09-07 17:50:35 +0200

    plugin/neogit: add keybind to push local commits

--- lua/valeth/plugins/neogit.lua
+++ lua/valeth/plugins/neogit.lua
@@ -7,8 +7,9 @@ spec.dependencies = {
 }
 
 spec.keys = {
-    { "<Leader>vs", "<cmd>Neogit<CR>", desc = "Open NeoGit" },
-    { "<Leader>vc", "<cmd>Neogit commit<CR>", desc = "Open NeoGit commit" },
+    { "<Leader>vs",  "<cmd>Neogit<CR>",        desc = "Open NeoGit" },
+    { "<Leader>vc",  "<cmd>Neogit commit<CR>", desc = "Open NeoGit commit" },
+    { "<Leader>vpu", "<cmd>!git push<CR>",     desc = "Push local commits to upstream" },
 }
 
 spec.config = true
