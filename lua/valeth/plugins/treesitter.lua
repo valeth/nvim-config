a6954a76c829a832129d7110d62509317d9b3ab6
Patrick Auernig <patrick.auernig@mykolab.com>
2024-10-06 14:28:39 +0200

    plugins/treesitter: add textobjects extension

--- lua/valeth/plugins/treesitter.lua
+++ lua/valeth/plugins/treesitter.lua
@@ -5,6 +5,7 @@ local spec = {
 
 spec.dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-textobjects",
     "RRethy/nvim-treesitter-endwise",
 }
 
@@ -34,7 +35,45 @@ spec.config = function()
             additional_vim_regex_highlighting = false,
         },
 
-        endwise = { enable = true }
+        endwise = { enable = true },
+
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["am"] = { query = "@function.outer", desc = "Select outer function" },
+                    ["im"] = { query = "@function.inner", desc = "Select inner function" },
+                    ["ab"] = { query = "@block.outer", desc = "Select outer block" },
+                    ["ib"] = { query = "@block.inner", desc = "Select inner block" },
+                },
+            },
+
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]m"] = { query = "@function.outer", desc = "Go to start of next function" },
+                    ["]b"] = { query = "@block.outer", desc = "Go to start of next block" },
+                    ["]s"] = { query = "@class.outer", desc = "Go to start of next struct-like" },
+                },
+                goto_next_end = {
+                    ["]M"] = { query = "@function.outer", desc = "Go to end of next function" },
+                    ["]B"] = { query = "@block.outer", desc = "Go to end of next block" },
+                    ["]S"] = { query = "@class.outer", desc = "Go to end of next struct-like" },
+                },
+                goto_previous_start = {
+                    ["[m"] = { query = "@function.outer", desc = "Go to start of previous function" },
+                    ["[b"] = { query = "@block.outer", desc = "Go to start of previous block" },
+                    ["[s"] = { query = "@class.outer", desc = "Go to start of previous struct-like" },
+                },
+                goto_previous_end = {
+                    ["[M"] = { query = "@function.outer", desc = "Go to end of previous function" },
+                    ["[B"] = { query = "@block.outer", desc = "Go to end of previous block" },
+                    ["[S"] = { query = "@class.outer", desc = "Go to end of previous struct-like" },
+                },
+            },
+        },
     })
 
     require("treesitter-context").setup()
