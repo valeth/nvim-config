3043c1d880475435bc17498d1f56105aeb60eb3e
Patrick Auernig <patrick.auernig@mykolab.com>
2025-08-21 16:40:24 +0200

    Disable neovide cursor animations

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("valeth.options")
 require("valeth.keymaps")
+require("valeth.neovide")
 
 if vim.version().minor >= 10 then
     require("valeth.lazy")
---
+++ lua/valeth/neovide.lua
@@ -0,0 +1,6 @@
+if not vim.g.neovide then
+    return
+end
+
+-- Disable cursor animation
+vim.g.neovide_cursor_animation_length = 0
