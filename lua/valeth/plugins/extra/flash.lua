277f740af8d73eea497988767a1b2a2fcd72692e
Patrick Auernig <patrick.auernig@mykolab.com>
2026-02-22 16:15:00 +0100

    Update flash keybinds
    s and S bindings cause some conflicts

--- lua/valeth/plugins/extra/flash.lua
+++ lua/valeth/plugins/extra/flash.lua
@@ -2,7 +2,7 @@ return {
     "folke/flash.nvim",
     opts = {},
     keys = {
-        { "s", mode = { "n" }, function() require("flash").jump() end,       desc = "Jump to location" },
-        { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Select area" },
+        { "<Leader>j", mode = { "n" }, function() require("flash").jump() end,       desc = "Jump to location" },
+        { "<Leader>s", mode = { "n" }, function() require("flash").treesitter() end, desc = "Select area" },
     },
 }
