bbca04dd9d9959f2507312b11882a5fdba2b346e
Patrick Auernig <patrick.auernig@mykolab.com>
2026-02-22 15:34:52 +0100

    Disable format on save by default and add toggle keymap

--- lua/valeth/plugins/extra/formatting.lua
+++ lua/valeth/plugins/extra/formatting.lua
@@ -1,5 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 local autogroup = vim.api.nvim_create_augroup
+local format_on_save = false
 
 local spec = {
     "mhartington/formatter.nvim",
@@ -18,20 +19,29 @@ spec.config = function()
 end
 
 local function format_file()
+    if not format_on_save then
+        return
+    end
+
     local bufnr = vim.api.nvim_get_current_buf()
     local lsp_client = vim.lsp.get_clients({ bufnr = bufnr })
 
     if #lsp_client ~= 0 then
-        vim.lsp.buf.format({ async = false, timeout_ms = 5000 })
+        vim.lsp.buf.format({ async = true, timeout_ms = 5000 })
     else
         vim.cmd(":FormatWrite")
     end
 end
 
+local function toggle_format_on_save()
+    format_on_save = not format_on_save
+end
+
 vim.keymap.set("n", "<Leader>fmt", format_file, { desc = "Format the current buffer" })
+vim.keymap.set("n", "<Leader>fos", toggle_format_on_save, { desc = "Toggle format on save" })
 
 -- Format on save
-autocmd("BufWritePost", {
+autocmd("BufWritePre", {
     group = autogroup("FormatOnSave", {}),
     callback = format_file,
 })
