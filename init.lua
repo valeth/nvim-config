db78bf4a191a46ce1a78f909ca0be54c8564d3ed
Patrick Auernig <patrick.auernig@mykolab.com>
2026-04-02 18:45:49 +0200

    Use neovim's version comparison function

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("valeth.options")
 require("valeth.keymaps")
 require("valeth.neovide")
 
-if vim.version().minor >= 12 then
+if vim.version.ge(vim.version(), "0.12") then
     require("valeth.plugins")
 else
     vim.notify("Plugins require Neovim version 0.12 or higher")
