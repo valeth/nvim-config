b1c83cc54466cc530ad7bd16116afd9affd265b7
Patrick Auernig <patrick.auernig@mykolab.com>
2026-04-05 19:32:35 +0200

    Enable experimental UI version 2

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("valeth.keymaps")
 require("valeth.neovide")
 
 if vim.version.ge(vim.version(), "0.12") then
+    require("vim._core.ui2").enable({})
     require("valeth.plugins")
 else
     vim.notify("Plugins require Neovim version 0.12 or higher")
