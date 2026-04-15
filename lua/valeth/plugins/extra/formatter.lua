4110daaf5ef9daf5338a77fd59bd09a5272b4f7e
Patrick Auernig <patrick.auernig@mykolab.com>
2026-04-15 13:15:56 +0200

    Bring back file formatting

--- lua/valeth/plugins/extra/formatter.lua
+++ lua/valeth/plugins/extra/formatter.lua
@@ -4,10 +4,6 @@ local autogroup = vim.api.nvim_create_augroup
 local format_on_save = false
 
 local function format_file()
-    if not format_on_save then
-        return
-    end
-
     local bufnr = vim.api.nvim_get_current_buf()
     local lsp_client = vim.lsp.get_clients({ bufnr = bufnr })
 
@@ -20,12 +16,18 @@ end
 
 local function toggle_format_on_save()
     format_on_save = not format_on_save
+    local status = format_on_save and "enabled" or "disabled"
+    vim.notify("Format on save " .. status, vim.log.levels.INFO)
 end
 
 -- Format on save
 autocmd("BufWritePre", {
     group = autogroup("FormatOnSave", {}),
-    callback = format_file,
+    callback = function()
+        if format_on_save then
+            format_file()
+        end
+    end
 })
 
 
@@ -38,5 +40,5 @@ require("formatter").setup({
 })
 
 
-vim.keymap.set("n", "<Leader>fmt", format_file, { desc = "Format the current buffer" })
-vim.keymap.set("n", "<Leader>fos", toggle_format_on_save, { desc = "Toggle format on save" })
+vim.keymap.set("n", "<Leader>fm", format_file, { desc = "Format the current buffer" })
+vim.keymap.set("n", "<Leader>fs", toggle_format_on_save, { desc = "Toggle format on save" })
