cfedd763881e89e204ce84cdd01dad2af041f4a6
Patrick Auernig <patrick.auernig@mykolab.com>
2026-04-25 21:41:05 +0200

    Prettier diagnostics signs

--- lua/valeth/options.lua
+++ lua/valeth/options.lua
@@ -25,6 +25,14 @@ vim.g.mapleader = " "
 
 vim.diagnostic.config({
     virtual_text = false,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "✘",
+            [vim.diagnostic.severity.WARN] = "▲",
+            [vim.diagnostic.severity.INFO] = "»",
+            [vim.diagnostic.severity.HINT] = "⚑",
+        }
+    }
 })
 
 augroup("valeth", { clear = true })
